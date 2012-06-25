dddc80cd17732d7d24a2b37a4edaff8e237ea665
KitKit <kitkit@keysocapp.com>
2012-06-25 22:41:32 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c1287fcf5771854bd72f1e2db8331887c68367d04bbf3a08cb7bb8dad658610f648685a7639c6a756051eff2ceb03722fd3d39902e0d76df5720de2cffc9e98a'
